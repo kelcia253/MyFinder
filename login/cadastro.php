744a76a4808aca2d90f6a2ea6a78b709b973b0d3
kelcia253 <110485657+kelcia253@users.noreply.github.com>
2023-02-27 16:48:12 -0300

    27/02

--- login/cadastro.php
+++ login/cadastro.php
@@ -6,42 +6,32 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistema de Cadastro - PHP + MySQL - Canal TI</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-    <link rel="stylesheet" href="css/bulma.min.css" />
-    <link rel="stylesheet" type="text/css" href="../login/css">
+    <title>Cadastre-se</title>
+    <link rel='stylesheet' type='text/css' media='screen' href='configuraçoes.css'>
+    <link rel = 'stylesheet' type = 'text/css' media='screen' href='../css/bootstrap.min.css'>
+    <script src='../js/bootstrap.bundle.min.js'></script>
+    <script src='main.js'></script>
+    <link rel="stylesheet" href="./loginCss/cadastro.css">
 </head>
  
-<body>
-    <section class="hero is-success is-fullheight">
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <div class="column is-4 is-offset-4">
-                    <h3 class="title has-text-grey">Sistema de Cadastro</h3>
-                    <div class="box">
-                        <form action="cadastrar.php" method="POST">
-                            <div class="field">
-                                <div class="control">
-                                    <input name="nome" type="text" class="input is-large" placeholder="Nome" autofocus>
-                                </div>
-                            </div>
-                            <div class="field">
-                                <div class="control">
-                                    <input name="email" type="text" class="input is-large" placeholder="Email">
-                                </div>
-                            </div>
-                            <div class="field">
-                                <div class="control">
-                                    <input name="senha" class="input is-large" type="password" placeholder="Senha">
-                                </div>
-                            </div>
-                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Cadastrar</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+<body class="form-signin">
+<form class="form-signin">
+    <div class="border border-dark p-2 mb-2 border-2 border" id="borda">
+        <h1 class="h3 mb-3 font-weight-normal align-self-center">Cadastre-se</h1>
+        <label for="inputNome" class="sr-only"></label> 
+        <input type="text" id="inputNome" class="form-control" placeholder="Nome" required autofocus>
+        <label for="inputEmail" class="sr-only"></label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Email" required>
+        <label for="inputPassword" class="sr-only"></label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+        <div id="botaologin">
+    <button class="btn btn-Lg btn-dark btn-block" type="submit">Enviar</button>
+    <button class="btn btn-Lg btn-dark btn-block" type="reset">Limpar</button>
+    </div>
+    <p class="mt-5 mb-3 text-muted">Desde 2023</p>
+    </form>   
+    </div>
+    
 </body>
  
 </html>
