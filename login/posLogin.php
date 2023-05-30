3326cd9e3aeb91f638107981815fc6265432786f
kelcia253 <k.kelcia.1981.1976.2006@gmail.com>
2023-05-30 15:01:16 -0300

    Lista

--- login/posLogin.php
+++ login/posLogin.php
@@ -109,7 +109,7 @@ include('conexao.php');
               </div>
               <div class="card-body">
               <img class="card-text" style="width: 250px; height:200px;" src="<?php echo $dados['imagem']; ?>" alt="Imagem do produto">
-              <p class="card-text"><?php echo $dados['preco']?></p>
+              <p class="card-text">Preço: <?php echo $dados['preco']?></p>
 
                 <p class="card-text"><?php echo $dados['descricao']?></p>
                
@@ -174,7 +174,7 @@ include('conexao.php');
               </div>
               <div class="card-body">
               <img class="card-text" style="width: 250px; height:200px;" src="<?php echo $dados['imagem']; ?>" alt="Imagem do produto">
-              <p class="card-text"><?php echo $dados['preco']?></p>
+              <p class="card-text">Preço: <?php echo $dados['preco']?></p>
 
                 <p class="card-text"><?php echo $dados['descricao']?></p>
                
