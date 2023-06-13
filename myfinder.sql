d76904442b547c5b3c2737f90ad65c7bffc3d8eb
kelcia253 <k.kelcia.1981.1976.2006@gmail.com>
2023-06-13 11:45:43 -0300

    bd

--- myfinder.sql
+++ myfinder.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Máquina: 127.0.0.1
--- Data de Criação: 13-Jun-2023 às 16:09
+-- Data de Criação: 13-Jun-2023 às 16:45
 -- Versão do servidor: 5.5.34
 -- versão do PHP: 5.4.22
 
@@ -56,14 +56,15 @@ CREATE TABLE IF NOT EXISTS `historicoprodutos` (
   `id_historico` int(100) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id_historico`),
   KEY `fk_id_produtos` (`id_produtos`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=38 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=40 ;
 
 --
 -- Extraindo dados da tabela `historicoprodutos`
 --
 
 INSERT INTO `historicoprodutos` (`nome`, `preco`, `data`, `id_produtos`, `id_historico`) VALUES
-('Frigobar Brastemp 76 Litros BRA08', '1.599,53', '2023-06-12', 32, 31);
+('Frigobar Brastemp 76 Litros BRA08', '1.599,53', '2023-06-12', 32, 31),
+('Cadeira Eames Eiffel ', '188,88', '2023-06-13', 36, 39);
 
 -- --------------------------------------------------------
 
@@ -81,14 +82,15 @@ CREATE TABLE IF NOT EXISTS `produtos` (
   `preco` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_produtos`),
   KEY `fk_id_cliente` (`fk_cliente`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=36 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=37 ;
 
 --
 -- Extraindo dados da tabela `produtos`
 --
 
 INSERT INTO `produtos` (`id_produtos`, `nome`, `descricao`, `link`, `imagem`, `fk_cliente`, `preco`) VALUES
-(32, 'Frigobar Brastemp 76 Litros BRA08', 'Com visual retrÃ´, o frigobar Brastemp 76 litros parece uma mini geladeira dos anos 50, um eletrodomÃ©stico super charmoso para armazenar menores quantidades de comida ou bebida em qualquer espaÃ§o da casa.  A parte interna do frigobar Brastemp tem compartimentos modulares, que podem ser posicionados como vocÃª achar melhor para a organizaÃ§Ã£o interna, compartimento extra frio, porta latas, gavetÃ£o e prateleira na porta para garrafas.  O modelo conta tambÃ©m com um congelador e recipiente para', 'https://www.zoom.com.br/frigobar/frigobar-bra08a-brastemp-retro-76-litros#lista-de-ofertas', 'imagensProduto/20230612215314-465884715.jpg', 7, '1.599,53');
+(32, 'Frigobar Brastemp 76 Litros BRA08', 'Com visual retrÃ´, o frigobar Brastemp 76 litros parece uma mini geladeira dos anos 50, um eletrodomÃ©stico super charmoso para armazenar menores quantidades de comida ou bebida em qualquer espaÃ§o da casa.  A parte interna do frigobar Brastemp tem compartimentos modulares, que podem ser posicionados como vocÃª achar melhor para a organizaÃ§Ã£o interna, compartimento extra frio, porta latas, gavetÃ£o e prateleira na porta para garrafas.  O modelo conta tambÃ©m com um congelador e recipiente para', 'https://www.zoom.com.br/frigobar/frigobar-bra08a-brastemp-retro-76-litros#lista-de-ofertas', 'imagensProduto/20230612215314-465884715.jpg', 7, '1.599,53'),
+(36, 'Cadeira Eames Eiffel ', 'A Cadeira Eames Eiffel Base Madeira - Preto Ã© um produto Ãºnico que alinha design e sofisticaÃ§Ã£o. Ela se adapta em ambientes modernos com uma decoraÃ§Ã£o arrojada e inovadora, podendo ser utilizada na sala de estar ou em ambientes externos. Com pÃ©s de madeira, Ã© uma Ã³tima opÃ§Ã£o para a decoraÃ§Ã£o da sua casa.  CaracterÃ­sticas principais: â€¢Moderna; â€¢Combina com diversos ambientes; â€¢Design Ãºnico; â€¢Encosto arredondado; â€¢Assento em polipropileno; â€¢PÃ©s em madeira; â€¢Peso supor', 'https://www.abracasa.com.br/cadeira-eames-eiffel-base-madeira-preta/p', 'imagensProduto/20230613164400cadeira.jpg', 6, '188,88');
 
 --
 -- Constraints for dumped tables
